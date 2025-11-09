33569ce8b27421f17b0ad0d8d264528a70fa92a7
NisargaChandra058 <nisargachandra79@gmail.com>
2025-11-09 20:57:02 +0530

    Update take-test.php

--- take-test.php
+++ take-test.php
@@ -1,6 +1,2 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-session_start(); // leave out session_save_path for now
-require_once('db.php');
+// require_once('db.php');
+echo "Hello, take-test is working!";
